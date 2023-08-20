4b3ee071116d4bdfced4b5bdbcf44bad251936c4
AhmadMattar <amattar56@gmail.com>
2023-08-20 22:48:49 +0300

    blogs-home-new-url2

--- stores/useSettings.ts
+++ stores/useSettings.ts
@@ -3,7 +3,7 @@ export const useSettingsStore = defineStore('useSettingsStore', {
         return {
             toastMessage: '',
             isToastFound: false,
-            domain: 'https://alqanoni.com//api',
+            domain: 'https://alqanouni.com///api',
             // notificationMessages:[] as any[],
             realTimeNotification: [] as any[],
             notificationMessages: new Map(),
